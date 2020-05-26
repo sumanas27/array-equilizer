1562979cf7c9ed662d8e727dd7916d2d0df04557
Sumana Saha <34478920+sumanas27@users.noreply.github.com>
2020-05-26 12:32:52 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        Map<Integer, Integer> intMap = new HashMap<>();
+        int max = 0;
+        if(arr.length >= 1 && arr.length <= 100){
+            List<Integer> list = new ArrayList<Integer>();
+            for (int item : arr)
+            {
+                list.add(item);
+            }
+            for(int item : arr) {
+                int frequency = Collections.frequency(list, item);
+                max = Math.max(max, frequency);
+            }
+        }
+
+        return arr.length - max;   
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
